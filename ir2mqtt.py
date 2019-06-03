f82b7393e84e1ad164c9ecc697fd7ad3cc85d487
rbausdor <robert.bausdorf@directline.de>
2019-06-03 16:24:37 +0200

    Hardened due to 24h LeMans experience

--- ir2mqtt.py
+++ ir2mqtt.py
@@ -1,4 +1,34 @@
 #!python3
+""" Daemon that can publishes iRacing telemetry values at MQTT topics.
+
+Configure what telemery values from iRacing you would like to publish at which
+MQTT topic.
+Calculate the geographical and astronomical correct light situation on track. 
+Send pit service flags and refuel amount to and receive pit commands from
+a buttonbox using a serial connection.
+ 
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__author__ = "Robert Bausdorf"
+__contact__ = "rbausdorf@gmail.com"
+__copyright__ = "2019, bausdorf engineering"
+#__credits__ = ["One developer", "And another one", "etc"]
+__date__ = "2019/06/01"
+__deprecated__ = False
+__email__ =  "rbausdorf@gmail.com"
+__license__ = "GPLv3"
+#__maintainer__ = "developer"
+__status__ = "Production"
+__version__ = "1.2"
 
 import irsdk
 import time
@@ -15,7 +45,6 @@ from datetime import time as dttime
 from datetime import tzinfo
 from irsdk import PitCommandMode
 
-version = 1.1
 debug = False
 
 # this is our State class, with some helpful variables
@@ -28,7 +57,8 @@ class State:
     latitude = -1
     longitude = -1
     elevation = -1
- 
+
+# Possible states given in Paho MQTT client callbacks 
 mqttRC = ['Connection successful', 
           'Connection refused - incorrect protocol version', 
           'Connection refused - invalid client identifier', 
@@ -52,6 +82,7 @@ def check_iracing():
         state.elevation = -1
         state.timezone = -1
 
+        # Close serial port to buttonbox
         if ser.is_open:
             ser.close()
 
@@ -60,6 +91,7 @@ def check_iracing():
         print('irsdk disconnected')
         mqtt_publish('state', 0)
     elif not state.ir_connected:
+        # Check if a dump file should be used to startup IRSDK
         if config.has_option('global', 'simulate'):
             is_startup = ir.startup(test_file=config['global']['simulate'])
             print('starting up using dump file: ' + str(config['global']['simulate']))
@@ -70,6 +102,7 @@ def check_iracing():
 
         if is_startup and ir.is_initialized and ir.is_connected:
             state.ir_connected = True
+            # Check need and open serial connection
             try:
                 if config.has_option('global', 'serial'):
                     ser.open()
@@ -78,24 +111,28 @@ def check_iracing():
                     
             print('irsdk connected')
             mqtt_publish('state', 1)
+            
+            # Get geographical track information and track timezone for
+            # astronomical calculations
             state.latitude = float(str(ir['WeekendInfo']['TrackLatitude']).rstrip(' m'))
             state.longitude = float(str(ir['WeekendInfo']['TrackLongitude']).rstrip(' m'))
             state.elevation = float(str(ir['WeekendInfo']['TrackAltitude']).rstrip(' m'))
             state.timezone = pytz.timezone(timeZoneFinder.certain_timezone_at(lng=state.longitude, lat=state.latitude))
-            
-            if ser.is_open:
-                writeSerialData()
 
 def publishSessionTime():
     
+    # Get the simulated time of day from IRSDK
     sToD = ir['SessionTimeOfDay']
     tod = time.localtime(float(sToD)-3600)
     dat = ir['WeekendInfo']['WeekendOptions']['Date'].split('-')
 
+    # Create a datetime object WITHOUT timezone info so it can be localized to
+    # the tracks timezone
     state.date_time = state.timezone.localize(datetime(int(dat[0]), int(dat[1]), int(dat[2]), tod.tm_hour, tod.tm_min, tod.tm_sec))
     # Display the current time in that time zone
     print('session ToD:', state.date_time.isoformat('T'))
 
+    # Publish using the timezone from configuration
     mqtt_publish('ToD', datetime.strftime(state.date_time.astimezone(pytz.timezone(config['mqtt']['timezone'])), "%Y-%m-%dT%H:%M:%S%z"))
     publishLightInfo(state.date_time)
 
@@ -104,6 +141,7 @@ def publishLightInfo(dateAndTime):
     if state.timezone is None:
         print("Could not determine the time zone")
     else:
+        # Calculate solar elevation and twilight start and end times
         angle = geoTime.solar_elevation(dateAndTime, state.latitude, state.longitude)
         print('solar elevation: ' + str(angle))
         mqtt_publish('solarElevation', str(angle))
@@ -116,6 +154,8 @@ def publishLightInfo(dateAndTime):
             print("DEBUG: setting start " + str(times_setting[0].astimezone(state.timezone)))
             print("DEBUG: setting end   " + str(times_setting[1].astimezone(state.timezone)))
 
+        # Classify and publish the current light situation on track as one of
+        # night, dawn, day or dusk  
         lightinfo = 'day'
         if dateAndTime < times_rising[0].astimezone(state.timezone):
             lightinfo = 'night'
@@ -133,39 +173,50 @@ def publishLightInfo(dateAndTime):
 
 
 def writeSerialData():
+    # Read pit flags from telemetry if it has changed and
+    # send it to serial port
     pit_svflags = ir['PitSvFlags']
     if state.pitFlags != pit_svflags:
         ser.write(('#PFL=' + str(pit_svflags) + '*').encode('ascii'))
         state.pitFlags = pit_svflags
         print('SERIAL> ' + '#PFL=' + str(pit_svflags) + '*')
 
+    # Read pit refuel amount from telemetry if it has changed and
+    # send it to serial port
     pit_svfuel = ir['PitSvFuel']
     if state.pitFuel != pit_svfuel:
         ser.write(('#PFU=' + str(pit_svfuel)[0:3] + '*').encode('ascii'))
         state.pitFuel = pit_svfuel
         print('SERIAL> ' + '#PFU=' + str(pit_svfuel)[0:3] + '*')
 
-def readSerialData(): 
-    telegram = str(ser.readline())
-    try:
-        start = telegram.index('#')
-        end = telegram.index('*')
-        telegram = telegram[start + 1:end]
-        print('SERIAL< ' + telegram)
-        keyvalue = telegram.split('=')
-            
-        if len(keyvalue) == 2:
-            if keyvalue[0] == 'PFU':
-                if debug:
-                    print('DEBUG: send fuel pit command ' + int(keyvalue[1]) + ' l')
-                ir.pit_command(PitCommandMode.fuel , int(keyvalue[1]))
-            if keyvalue[0] == 'PCM':
-                if debug:
-                    print('DEBUG: send pit command ' + int(keyvalue[1]))
-                ir.pit_command(int(keyvalue[1]))
+def readSerialData():
+    try: 
+        # Check if data is available on serial port
+        telegram = str(ser.readline())
+    
+        try:
+            # Determine the telegram part in serial data
+            start = telegram.index('#')
+            end = telegram.index('*')
+            telegram = telegram[start + 1:end]
+            print('SERIAL< ' + telegram)
+            keyvalue = telegram.split('=')
+    
+            # Check if telegram key and send the appropriate pit command to iRacing
+            if len(keyvalue) == 2:
+                if keyvalue[0] == 'PFU':
+                    if debug:
+                        print('DEBUG: send fuel pit command ' + int(keyvalue[1]) + ' l')
+                    ir.pit_command(PitCommandMode.fuel , int(keyvalue[1]))
+                if keyvalue[0] == 'PCM':
+                    if debug:
+                        print('DEBUG: send pit command ' + int(keyvalue[1]))
+                    ir.pit_command(int(keyvalue[1]))
+        except Exception as e:
+            if debug:
+                print('DEBUG: error processing telegram ' + telegram + ' : ' + str(e))
     except Exception as e:
-        if debug:
-            print('DEBUG: error processing telegram ' + telegram + ' : ' + e)
+        print('Error processing telegram: ' + str(e))
 
 # our main loop, where we retrieve data
 # and do something useful with it
@@ -179,9 +230,11 @@ def loop():
     ir.freeze_var_buffer_latest()
 
     state.tick += 1
+    # publish session time and configured telemetry values every minute
     if state.tick % 60 == 1:
         publishSessionTime()
     
+        # read and publish configured telemetry values
         if config.has_section('iracing'):
             for top in config['iracing']:
                 ind = config.get('iracing', top).split('/')
@@ -192,10 +245,11 @@ def loop():
                             val = val[0].__getitem__(key)
                         else:
                             val = val.__getitem__(key)
-
+    
                 if val != None:
                     mqtt_publish(top, val)
-    
+
+    # Read/Write serial data as needed
     if useSerial and ser.is_open:
         writeSerialData()
         readSerialData()
@@ -206,7 +260,8 @@ def mqtt_publish(topic, data):
     mqttClient.publish(top, data)
     if debug:
         print('DEBUG mqtt_publish(' + top + ', ' + str(data) + ')')
-    
+
+# Paho MQTT callback 
 def on_connect(client, userdata, flags, rc):
     print('MQTT: ' + mqttRC[rc])
     if rc==0:
@@ -217,6 +272,7 @@ def on_connect(client, userdata, flags, rc):
     else:
         print("Bad connection Returned code=",rc)
 
+# Paho MQTT callback
 def on_disconnect(client, userdata, rc):
     if rc==0:
         print('MQTT: connection terminated')
@@ -226,19 +282,22 @@ def on_disconnect(client, userdata, rc):
 def banner():
     print("=============================")
     print("|         IR2MQTT           |")
-    print("|           " + str(version) + "             |")
+    print("|           " + str(__version__) + "             |")
     print("=============================")
     print("MQTT host: " + config['mqtt']['host'])
     print("MQTT port: " + config['mqtt']['port'])
     print("MQTT base: " + config['mqtt']['baseTopic'])
 
+# Here is our main program entry
 if __name__ == '__main__':
+    # Read configuration file
     config = configparser.ConfigParser()    
     try: 
         config.read('ir2mqtt.ini')
     except Exception:
         print('unable to read configuration: ' + Exception.__cause__)
 
+    # Print banner an debug output status
     banner()
     if config.has_option('global', 'debug'):
         debug = config.getboolean('global', 'debug')
@@ -246,10 +305,11 @@ if __name__ == '__main__':
     if debug:
         print('Debug output enabled')
 
-    ir = irsdk.IRSDK()
     # initializing ir and state
+    ir = irsdk.IRSDK()
     state = State()
-    
+
+    # Initialize and connect MQTT client to configured broker
     mqttClient = mqtt.Client("irClient")
     mqttClient.on_connect=on_connect
     mqttClient.on_disconnect=on_disconnect
@@ -259,6 +319,7 @@ if __name__ == '__main__':
     except Exception:
         print('unable to connect to mqtt broker')
 
+    # Initialize and configure serial port
     ser = serial.Serial()
     useSerial = False
     if config.has_option('global', 'serial'):
@@ -268,10 +329,11 @@ if __name__ == '__main__':
         useSerial = True
         print('using COM port: ' + str(ser.port))
 
+    # Initialize astronomical calculator and timezone finder
     geoTime = astral.Astral()
     geoTime.solar_depression = 'civil'
     timeZoneFinder = timezonefinder.TimezoneFinder()
-    
+
     try:
         # infinite loop
         while True:
